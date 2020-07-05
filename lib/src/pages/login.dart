5a3dee58128dab500c8c786fddee077a77a11ea6
Guix98 <guimega.gh@gmail.com>
2020-07-05 00:18:42 -0400

    tabs poara separar pedidos

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -2,10 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 class Login extends StatefulWidget {
-  
   @override
-  
   _LoginState createState() => _LoginState();
 }
 
@@ -13,25 +12,23 @@ class _LoginState extends State<Login> {
   String nombre = "";
   String email = "";
   String password = "";
-  String url = 'http://192.168.31.200:8008/api/v1/security/login';
+  String url = 'http://192.168.0.14:8008/api/v1/security/login';
 
-  Future<String> getresp(String username, String password) async{
-
-    Map<String, dynamic> loginbody ={
-      "username" :  username,
-      "password" : password,
-    
+  Future<String> getresp(String username, String password) async {
+    Map<String, dynamic> loginbody = {
+      "username": username,
+      "password": password,
     };
     String body = jsonEncode(loginbody);
-    Map<String,String> headers ={
+    Map<String, String> headers = {
       "Accept": "application/json",
       "Content-Type": "application/json"
     };
-    var resp = await http.post(url,body: body, headers:  headers);
-    
-    Map<String, dynamic>responseMap= jsonDecode(resp.body);
+    var resp = await http.post(url, body: body, headers: headers);
+
+    Map<String, dynamic> responseMap = jsonDecode(resp.body);
     print(responseMap["message"]);
-    if (responseMap["message"]=='Authentication OK') {
+    if (responseMap["message"] == 'Authentication OK') {
       Navigator.pushNamed(context, 'sesion');
     } else {
       Alerta(context);
@@ -40,38 +37,36 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    
     return ListView(
-        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-        children: <Widget>[
-          _crearInput(),
-          Divider(),
-
-          _crearPassword(),
-          Divider(),
-          FlatButton(onPressed: (){
-                    getresp(nombre, password);
-                    
-          }, 
-                    child: Text('Ingresar'), color: Colors.red,)
-        ],
-      );}
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      children: <Widget>[
+        _crearInput(),
+        Divider(),
+        _crearPassword(),
+        Divider(),
+        FlatButton(
+          onPressed: () {
+            getresp(nombre, password);
+          },
+          child: Text('Ingresar'),
+          color: Colors.red,
+        )
+      ],
+    );
+  }
 
   Widget _crearInput() {
     return TextField(
       textCapitalization: TextCapitalization.none,
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
         counter: Text('Palabras: ${nombre.length}'),
         suffixIcon: Icon(Icons.insert_emoticon),
         hintText: 'Nombre de Usuario',
         labelText: 'Usuario',
         icon: Icon(Icons.keyboard),
       ),
-      onChanged: (valor){
-        
+      onChanged: (valor) {
         setState(() {
           nombre = valor;
         });
@@ -84,46 +79,43 @@ class _LoginState extends State<Login> {
       obscureText: true,
       keyboardType: TextInputType.visiblePassword,
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
         counter: Text('Palabras: ${password.length}'),
         suffixIcon: Icon(Icons.lock_outline),
         hintText: 'Contrasena',
         labelText: 'Password',
-        
         icon: Icon(Icons.keyboard),
       ),
-      onChanged: (valor){
-        
+      onChanged: (valor) {
         setState(() {
           password = valor;
         });
       },
     );
   }
+
   void Alerta(BuildContext context) {
     showDialog(
-      barrierDismissible: true,
-      context: context,
-      builder: (context){
-      return AlertDialog(
-        title: Text('Error'),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Usuario o password incorrecto'),
-            
-          ],
-        ),
-        
-        elevation: 10.0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-        actions: <Widget>[
-          
-          FlatButton(onPressed: ()=> Navigator.of(context).pop(), child: Text('Ok')),
-        ],
-      );
-      });
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Usuario o password incorrecto'),
+              ],
+            ),
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Ok')),
+            ],
+          );
+        });
   }
 }
--- lib/src/pages/sesion.dart
+++ lib/src/pages/sesion.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
-class SesionPage extends StatelessWidget {
-  
 
+class SesionPage extends StatefulWidget {
+  @override
+  _SesionPageState createState() => _SesionPageState();
+}
+
+class _SesionPageState extends State<SesionPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Bienvenido')),
-      body: ListView(
-        children: <Widget>[
-          ListTile(title: Text('Usuario 1'), subtitle: Text('Desc'),),
-          Divider(),
-          ListTile(title: Text('Usuario 1'), subtitle: Text('Desc'),),
-          Divider(),
-          ListTile(title: Text('Usuario 1'), subtitle: Text('Desc'),),
-          Divider(),
-          ListTile(title: Text('Usuario 1'), subtitle: Text('Desc'),),
-          Divider(),
-        ],
-      )
-      );
-    
+    return MaterialApp(
+      title: 'Fakeflix',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, accentColor: Colors.red),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "Pagados", icon: Icon(Icons.payment)),
+                Tab(text: "Preparados", icon: Icon(Icons.assignment_turned_in)),
+                Tab(text: "Despachados", icon: Icon(Icons.airport_shuttle)),
+                Tab(
+                    text: "Entregados",
+                    icon: Icon(Icons.sentiment_very_satisfied)),
+              ],
+            ),
+            title: Text('Pedidos'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.payment),
+              Icon(Icons.assignment_turned_in),
+              Icon(Icons.airport_shuttle),
+              Icon(Icons.sentiment_very_satisfied)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget pagados() {
+    return ListView(
+      padding: EdgeInsets.all(8),
+      children: <Widget>[
+        ListTile(),
+      ],
+    );
   }
 }
